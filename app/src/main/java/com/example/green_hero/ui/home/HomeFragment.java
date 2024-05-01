a5a1cf248838eb595b7b1e95cf8c02afd991cf08
Dimitris Sparagis <dimsparagis@gmail.com>
2024-05-01 11:47:33 +0300

    Finished Layout for Profile Fragment

--- app/src/main/java/com/example/green_hero/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/green_hero/ui/home/HomeFragment.java
@@ -16,6 +16,7 @@ public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
 
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         HomeViewModel homeViewModel =
