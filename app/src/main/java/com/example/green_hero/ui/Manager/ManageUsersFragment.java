72b35b51e178acd039fb2ed564b182a2a48fd28b
John Sfyrakis <75318233+JohnSfy@users.noreply.github.com>
2024-05-07 17:15:46 +0300

    managers page created

--- app/src/main/java/com/example/green_hero/ui/Manager/ManageUsersFragment.java
+++ app/src/main/java/com/example/green_hero/ui/Manager/ManageUsersFragment.java
@@ -1,7 +1,18 @@
 package com.example.green_hero.ui.Manager;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
+import com.example.green_hero.R;
 
 public class ManageUsersFragment extends Fragment {
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_manage_users, container, false);
+    }
 }
+
