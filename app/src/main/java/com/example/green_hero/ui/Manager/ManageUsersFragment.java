fe64a1cdb84ed8b9496a6697b3df983341501389
JohnSfy <you@example.com>
2024-05-12 17:06:43 +0300

    Buttons Accept Reject just created without fuctionality Button nav fixed not covering the fragment special thanks to Dimitris Sparagis.

--- app/src/main/java/com/example/green_hero/ui/Manager/ManageUsersFragment.java
+++ app/src/main/java/com/example/green_hero/ui/Manager/ManageUsersFragment.java
@@ -5,18 +5,38 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
+import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.example.green_hero.R;
+import com.example.green_hero.databinding.FragmentProfileBinding;
+
+import android.view.Window;
+import androidx.activity.EdgeToEdge;
 
 public class ManageUsersFragment extends Fragment {
+
+    private FragmentProfileBinding binding;
+
+    @Nullable
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        View bottomNavigationView = getActivity().findViewById(R.id.nav_view);
+
         View view = inflater.inflate(R.layout.fragment_manage_users, container, false);
+
+        //Padding to make the bottom navigation bar not overlap with the content
+        bottomNavigationView.post(() -> {
+            int height = bottomNavigationView.getHeight();
+            view.setPadding(view.getPaddingLeft(), view.getPaddingTop(), view.getPaddingRight(), height);
+        });
+
+
+
         return view;
     }
-
 }
+
 
