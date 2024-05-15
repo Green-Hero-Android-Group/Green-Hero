51e0007e18c77cb7564264fc605eaec940e23a46
Dimitris Sparagis <dimsparagis@gmail.com>
2024-05-15 20:11:59 +0300

    Created Model Classes (Admin, Recycle)

--- app/src/main/java/com/example/green_hero/model/Admin/Request.java
+++ app/src/main/java/com/example/green_hero/model/Admin/Request.java
@@ -2,7 +2,44 @@ package com.example.green_hero.model.Admin;
 
 import com.example.green_hero.model.User.ClassicUser;
 
-public class Request {
+import org.bson.types.ObjectId;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class Request extends RealmObject {
+    @PrimaryKey
+    private ObjectId _id;
     private ClassicUser user;
     private String status;
+
+    public Request(ObjectId _id, ClassicUser user, String status) {
+        this._id = _id;
+        this.user = user;
+        this.status = status;
+    }
+
+    public ObjectId get_id() {
+        return _id;
+    }
+
+    public void set_id(ObjectId _id) {
+        this._id = _id;
+    }
+
+    public ClassicUser getUser() {
+        return user;
+    }
+
+    public void setUser(ClassicUser user) {
+        this.user = user;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
--- app/src/main/java/com/example/green_hero/model/Recycle/Item.java
+++ app/src/main/java/com/example/green_hero/model/Recycle/Item.java
@@ -1,4 +1,56 @@
 package com.example.green_hero.model.Recycle;
 
-public class Item {
+import org.bson.types.ObjectId;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class Item extends RealmObject {
+    @PrimaryKey
+    private ObjectId _id;
+    private String name;
+    private int quantity;
+    private String type;
+
+    public Item() {
+    }
+
+    public Item(ObjectId _id, String name, int quantity, String type) {
+        this._id = _id;
+        this.name = name;
+        this.quantity = quantity;
+        this.type = type;
+    }
+
+    public ObjectId get_id() {
+        return _id;
+    }
+
+    public void set_id(ObjectId _id) {
+        this._id = _id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
