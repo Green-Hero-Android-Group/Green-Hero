16e8b433fcae73ccb12966cb4e068afdb0d47dc3
Dimitris Sparagis <dimsparagis@gmail.com>
2024-06-04 18:27:52 +0300

    Fixed issue with leaderboard in Stats: Some of the players weren't in the leaderboard

--- app/src/main/java/com/example/green_hero/ui/stats/StatsFragment.java
+++ app/src/main/java/com/example/green_hero/ui/stats/StatsFragment.java
@@ -21,8 +21,14 @@ import com.example.green_hero.model.User.ClassicUser;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.temporal.TemporalAdjusters;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import io.realm.RealmList;
+import io.realm.RealmResults;
+import io.realm.mongodb.User;
 
 public class StatsFragment extends Fragment {
     private FragmentStatsBinding binding;
@@ -40,8 +46,8 @@ public class StatsFragment extends Fragment {
         View root = binding.getRoot();
 
         //DB
-        RealmList<ClassicUser> users = DB.users;
-        RealmList<Recycle> recycles = DB.recycles;
+        RealmResults<ClassicUser> users = DB.realm.where(ClassicUser.class).findAll();
+        RealmResults<Recycle> recycles = DB.realm.where(Recycle.class).findAll();
         int paperCounter = 0;
         int plasticCounter = 0;
         int glassCounter = 0;
@@ -49,6 +55,10 @@ public class StatsFragment extends Fragment {
         //Updating the UI
         topUser = binding.hotwName;
         topUserRecycles = binding.hothRecycles;
+        recycledItems = binding.numOfRecycledItems;
+        topMaterial = binding.topMaterial;
+        topMaterialRecycles = binding.topMaterialRecycles;
+
         LocalDate today = LocalDate.now();
         DayOfWeek dayOfWeek = today.getDayOfWeek();
         LocalDate monday = today.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
@@ -58,6 +68,7 @@ public class StatsFragment extends Fragment {
         ClassicUser hero = null;
         for (ClassicUser user : users) {
             if (!user.getName().equals("Admin")) {
+                System.out.println(user.getName());
                 RealmList<Recycle> currentWeekRecycles = new RealmList<>();
                 System.out.println(user.getName());
                 for (Recycle recycle : user.getRecycles()) {
@@ -72,70 +83,75 @@ public class StatsFragment extends Fragment {
                 }
             }
         }
+        if (users.size() == 1 || recycles.isEmpty()) {
+            topUser.setText("No users");
+            topUserRecycles.setText("No recycles");
+            recycledItems.setText("0");
+            topMaterial.setText("None");
+            topMaterialRecycles.setText("0");
+        } else {
 
-        topUser.setText(hero.getName().toString());
-        topUserRecycles.setText("with " + String.valueOf(maxRecycles) + " recycles");
+            topUser.setText(hero.getName().toString());
+            topUserRecycles.setText("with " + String.valueOf(maxRecycles) + " recycles");
 
 
-        recycledItems = binding.numOfRecycledItems;
-        recycledItems.setText(String.valueOf(recycles.size()));
+            recycledItems.setText(String.valueOf(recycles.size()));
 
-        topMaterial = binding.topMaterial;
-        topMaterialRecycles = binding.topMaterialRecycles;
-        for (Recycle recycle : recycles) {
-            if (recycle.getItem().getType().equals("Paper")) {
-                paperCounter++;
-            } else if (recycle.getItem().getType().equals("Plastic")) {
-                plasticCounter++;
-            } else if (recycle.getItem().getType().equals("Glass")) {
-                glassCounter++;
-            }
-        }
-
-        String topMaterialString = "";
-        int max = 0;
-        for (int i = 0; i < 2; i++) {
-            if (paperCounter > max) {
-                max = paperCounter;
-                topMaterialString = "Paper";
-            }
-            if (plasticCounter > max) {
-                max = plasticCounter;
-                topMaterialString = "Plastic";
-            }
-            if (glassCounter > max) {
-                max = glassCounter;
-                topMaterialString = "Glass";
-            }
-        }
-        topMaterial.setText(topMaterialString);
-        topMaterialRecycles.setText(String.valueOf(max));
-
-        tableLayout = binding.leaderboard;
-        RealmList<ClassicUser> orderedUsers = new RealmList<>();
-        for (ClassicUser user : users) {
-            if (!user.getName().equals("Admin")) {
-                if (orderedUsers.isEmpty()) {
-                    orderedUsers.add(user);
-                } else {
-                    for (int i = 0; i < orderedUsers.size(); i++) {
-                        if (user.getRecycles().size() > orderedUsers.get(i).getRecycles().size()) {
-                            orderedUsers.add(i, user);
-                            break;
-                        }
-                    }
+            for (Recycle recycle : recycles) {
+                if (recycle.getItem().getType().equals("Paper")) {
+                    paperCounter++;
+                } else if (recycle.getItem().getType().equals("Plastic")) {
+                    plasticCounter++;
+                } else if (recycle.getItem().getType().equals("Glass")) {
+                    glassCounter++;
                 }
             }
-        }
-        for (ClassicUser user : orderedUsers) {
-            TableRow row = (TableRow) inflater.inflate(R.layout.leaderboard_row, container, false);
-            TextView name = row.findViewById(R.id.heroName);
-            TextView score = row.findViewById(R.id.heroScore);
-            TextView rank = row.findViewById(R.id.heroRank);
-            name.setText(user.getName());
-            score.setText(String.valueOf(user.getRecycles().size()));
-            rank.setText(String.valueOf(orderedUsers.indexOf(user) + 1));
-            tableLayout.addView(row);
+
+            String topMaterialString = "";
+            int max = 0;
+            for (int i = 0; i < 2; i++) {
+                if (paperCounter > max) {
+                    max = paperCounter;
+                    topMaterialString = "Paper";
+                }
+                if (plasticCounter > max) {
+                    max = plasticCounter;
+                    topMaterialString = "Plastic";
+                }
+                if (glassCounter > max) {
+                    max = glassCounter;
+                    topMaterialString = "Glass";
+                }
+            }
+            topMaterial.setText(topMaterialString);
+            topMaterialRecycles.setText(String.valueOf(max));
+
+            tableLayout = binding.leaderboard;
+            RealmResults<ClassicUser> orderedUsers = DB.realm.where(ClassicUser.class).findAll();
+            Comparator<ClassicUser> compareByRecycles = (user1, user2) -> Integer.compare(user2.getRecycles().size(),
+                    user1.getRecycles().size());
+
+            // Create a new ArrayList to store sorted users
+            ArrayList<ClassicUser> sortedUsers = new ArrayList<>(orderedUsers);
+
+            // Sort the new ArrayList using the comparator
+            Collections.sort(sortedUsers, compareByRecycles);
+
+            // Print the sorted users (users with most recycles first)
+            for (ClassicUser user : sortedUsers) {
+                System.out.println("User recycles: " + user.getRecycles().size());
+            }
+            for (ClassicUser user : sortedUsers) {
+                System.out.println(user.getName());
+                TableRow row = (TableRow) inflater.inflate(R.layout.leaderboard_row, container, false);
+                TextView name = row.findViewById(R.id.heroName);
+                TextView score = row.findViewById(R.id.heroScore);
+                TextView rank = row.findViewById(R.id.heroRank);
+                name.setText(user.getName());
+                score.setText(String.valueOf(user.getRecycles().size()));
+                rank.setText(String.valueOf(orderedUsers.indexOf(user) + 1));
+                tableLayout.addView(row);
+            }
         }
 
         return root;
