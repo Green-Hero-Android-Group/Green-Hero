e10828b086a82d512a15db10e88f9e95848acd53
Dimitris Sparagis <dimsparagis@gmail.com>
2024-06-06 12:19:46 +0300

    Added History in Home page

--- app/src/main/java/com/example/green_hero/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/green_hero/ui/home/HomeFragment.java
@@ -16,19 +16,26 @@ import androidx.lifecycle.ViewModelProvider;
 import com.example.green_hero.DB;
 import com.example.green_hero.R;
 import com.example.green_hero.databinding.FragmentHomeBinding;
+import com.example.green_hero.model.Recycle.Recycle;
 import com.example.green_hero.model.User.ClassicUser;
 import com.example.green_hero.model.User.Trophy;
 import com.example.green_hero.utils.Actions;
 import com.example.green_hero.utils.Transactions;
 
 import org.bson.types.ObjectId;
+import org.w3c.dom.Text;
 
+import io.realm.RealmList;
 import io.realm.RealmResults;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
-    TextView helloMessage;
+    private TextView helloMessage;
+    private TextView historyDate1;
+    private TextView historyDate2;
+    private TextView historyRecycle1;
+    private TextView historyRecycle2;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -69,6 +76,29 @@ public class HomeFragment extends Fragment {
         //Update user's name in Hello message
         helloMessage = binding.helloMessage;
         helloMessage.setText("Hello, " + user.getName() + "!");
+
+        //Updating history
+        historyDate1 = binding.historyDate1;
+        historyDate2 = binding.historyDate2;
+        historyRecycle1 = binding.historyRecycle1;
+        historyRecycle2 = binding.historyRecycle2;
+
+        //Getting the 2 most recent recycles
+        int recycleSize = user.getRecycles().size();
+        if (recycleSize == 1) {
+            RealmList<Recycle> oneRecycle = new RealmList<>();
+            oneRecycle.add(user.getRecycles().get(0));
+        } else if( recycleSize > 1) {
+            RealmList<Recycle> twoRecycles = new RealmList<>();
+            twoRecycles.add(user.getRecycles().get(recycleSize - 1));
+            twoRecycles.add(user.getRecycles().get(recycleSize - 2));
+
+            historyDate1.setText(twoRecycles.get(0).getDate());
+            historyDate2.setText(twoRecycles.get(1).getDate());
+            historyRecycle1.setText(Integer.toString(twoRecycles.get(1).getItem().getQuantity()) + " " + twoRecycles.get(1).getItem().getName());
+            historyRecycle2.setText(Integer.toString(twoRecycles.get(0).getItem().getQuantity()) + " " + twoRecycles.get(0).getItem().getName());
+        }
+
 
         return root;
     }
